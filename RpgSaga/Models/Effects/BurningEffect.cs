defebe2e260dd6ff3739206f9210ff53b5995569
Lik-i-l <nik28092000@yandex.ru>
2026-05-25 16:12:35 +0300

    Create BurningEffect.cs

---
+++ RpgSaga/Models/Effects/BurningEffect.cs
@@ -0,0 +1,24 @@
+using RpgSaga.Models.Enums;
+
+namespace RpgSaga.Models.Effects;
+
+public class BurningEffect : IEffect
+{
+    public EffectType Type => EffectType.Burning;
+    public int RemainingTurns { get; set; }
+    public int DamagePerTurn => 2;
+    public ElementType Element => ElementType.Fire;
+
+    public BurningEffect(int duration = 3)
+    {
+        RemainingTurns = duration;
+    }
+
+    public void Apply(Hero target)
+    {
+        target.TakeDamage(DamagePerTurn, Element);
+        RemainingTurns--;
+    }
+
+    public IEffect Clone() => new BurningEffect(RemainingTurns);
+}
