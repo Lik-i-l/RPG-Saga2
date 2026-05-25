0f6731275155b473f38b56645fe578a50da7c0ef
Lik-i-l <nik28092000@yandex.ru>
2026-05-25 16:15:12 +0300

    Create RetributionStrike.cs

---
+++ RpgSaga/Models/Abilities/RetributionStrike.cs
@@ -0,0 +1,18 @@
+using RpgSaga.Models.Heroes;
+
+namespace RpgSaga.Models.Abilities;
+
+public class RetributionStrike : IAbility
+{
+    public string Name => "Удар возмездия";
+
+    public string Execute(Hero caster, Hero target)
+    {
+        int bonusDamage = (int)(caster.Strength * 0.3);
+        int totalDamage = caster.Strength + bonusDamage;
+        target.TakeDamage(totalDamage, ElementType.None);
+        return $"{caster.Name} использует {Name} и наносит урон {totalDamage} противнику {target.Name}";
+    }
+
+    public int GetDamage() => 0;
+}
