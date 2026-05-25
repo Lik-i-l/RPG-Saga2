bccad8a94ebe8ca082e724ae023be76eb77ad2bb
Lik-i-l <nik28092000@yandex.ru>
2026-05-25 16:14:51 +0300

    Create MeleeAttack.cs

---
+++ RpgSaga/Models/Abilities/MeleeAttack.cs
@@ -0,0 +1,17 @@
+using RpgSaga.Models.Heroes;
+
+namespace RpgSaga.Models.Abilities;
+
+public class MeleeAttack : IAbility
+{
+    public string Name => "Обычная атака";
+
+    public string Execute(Hero caster, Hero target)
+    {
+        int damage = caster.Strength;
+        target.TakeDamage(damage, ElementType.None);
+        return $"{caster.Name} наносит урон {damage} противнику {target.Name}";
+    }
+
+    public int GetDamage() => 0;
+}
